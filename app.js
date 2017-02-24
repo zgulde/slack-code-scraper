8bcb35dba1b7b03bea16a7deafe811c92f9190fd
Zach Gulde <zachgulde@gmail.com>
2017-02-23 19:58:32 -0600

    ping the db every hour
    so mysql doesn't timeout the connection on us

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const bot = require('./src/createBot.js');
-const { shouldIgnoreEvent, containsCode } = require('./src/eventUtils.js');
+const bot = require('./src/createBot');
+const dbc = require('./src/dbc');
+const { shouldIgnoreEvent, containsCode, save } = require('./src/eventUtils.js');
 
 bot.on('start', () => {
   console.log('Bot started!');
@@ -9,8 +10,26 @@ bot.on('message', event => {
   if (shouldIgnoreEvent(event)) {
     return;
   } else if (containsCode(event)) {
-    // save(event);
-    console.log('found some code!');
-    console.log(event);
+    save(event, id => {
+      console.log(`inserted code sample # ${id}!`);
+    });
   }
 });
+
+/**
+ * mysql will close a connection if it is not active for a certain amount of
+ * time (8 hours by default), this would kill our application, so we will
+ * ping the server every so often to keep the connection up and healthy
+ **/
+const PING_TIME = ( // every hour
+  1000 * // ms -> seconds
+  60 *   // seconds -> minutes
+  60     // minutes -> hours
+);
+setInterval(() => {
+  dbc.ping(err => {
+    if (err) throw err;
+    console.log('Server responded to ping');
+  });
+}, PING_TIME);
+
